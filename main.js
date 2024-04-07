cdd9d56e352e992978f2d7b7a3569e373d2c7d1d
wallop560 <lukesupreem@gmail.com>
2024-04-07 04:57:04 +0100

    Added Previous work Section

--- main.js
+++ main.js
@@ -1,10 +1,19 @@
 const observer = new IntersectionObserver((entries) => {
     entries.forEach((entry) => {
-        console.log(entry)
-        if (entry.isIntersecting){
+        if (entry.isIntersecting) {
             entry.target.classList.add('show');
-        }else{
-            entry.target.classList.remove("show");
+            if (entry.target.dataset.blur === 'blurred') {
+                entry.target.style.filter = entry.target.dataset.previousBlur;
+            } else {
+                entry.target.style.filter = 'none'; // Remove blur
+            }
+        } else {
+            entry.target.classList.remove('show');
+            if (entry.target.style.filter && entry.target.style.filter !== 'none') {
+                entry.target.dataset.blur = 'blurred';
+                entry.target.dataset.previousBlur = entry.target.style.filter;
+            }
+            entry.target.style.filter = 'blur(2px)'; // Apply blur when hidden
         }
     });
 });
